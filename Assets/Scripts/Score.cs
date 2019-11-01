4dc7d857990e8320f8746e428355d0bec21382c5
Scott <cds229@om106b19.oldmain.txstate.edu>
2019-11-01 13:50:29 -0500

    11/1 commit Pong

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Score : MonoBehaviour
+{
+    public int countLeft = 0,
+               countRight = 0;
+    
+    public GameObject scoreLeft;    //left side of table
+    public GameObject scoreRight;    //right side of table
+
+    void OnCollisionEnter(Collision hit)
+    {
+        if (hit.gameObject.CompareTag("Ball") )
+            countLeft++;
+        
+        else if (hit.gameObject.CompareTag("Ball") )
+            countRight++;
+        
+        Debug.Log(countLeft);
+        Debug.Log(countRight);
+    }
+}
